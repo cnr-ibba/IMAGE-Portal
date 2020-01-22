b14cdfc3bbb9c856bfde752b0b8fde8961384bcd
Alexey Sokolov <alexey@ebi.ac.uk>
2020-01-22 12:01:30 +0000

    Adding new breeds to front-end

--- IMAGE-frontend/src/app/tables/organisms/species.ts
+++ IMAGE-frontend/src/app/tables/organisms/species.ts
@@ -1,13 +1,15 @@
 export const speciesNames = {
   'Oryctolagus cuniculus': 'Rabbit',
   'Anser anser': 'Goose (domestic)',
-  'Canis lupus familiaris': 'Dog'
+  'Canis lupus familiaris': 'Dog',
+  'Ovis aries': 'Sheep'
 };
 
 export const breedsNames = {
   'Oryctolagus cuniculus': ['Gouwenaar', 'Havana', 'Ekster', 'Hulstlander konijn', 'Beige', 'Sallander', 'Deilenaar', 'Thrianta konijn'],
   'Anser anser': ['Twentse landgans'],
-  'Canis lupus familiaris': ['Stabijhoun', 'Wetterhoun']
+  'Canis lupus familiaris': ['Stabijhoun', 'Wetterhoun'],
+  'Ovis aries': ['Flevolander']
 };
 
 export const specialBreedCases = {
