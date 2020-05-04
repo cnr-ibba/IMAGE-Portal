1ee2d47c2f08c7cb883d14824cfb7ce14af51a1a
Paolo Cozzi <bunop@libero.it>
2020-05-04 17:42:45 +0200

    :recycle: construct api help urls using environment

--- IMAGE-frontend/src/app/help/api/api.component.ts
+++ IMAGE-frontend/src/app/help/api/api.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HostSetting } from '../../tables/host-setting';
+
 @Component({
   selector: 'app-api',
   templateUrl: './api.component.html',
-  styleUrls: ['./api.component.css']
+  styleUrls: ['./api.component.css'],
+  // define providers since HostSetting isn't provided in app.module.ts
+  providers: [ HostSetting ]
 })
 export class ApiComponent implements OnInit {
+  private backend: string;
 
-  constructor() { }
+  constructor(private hostSetting: HostSetting) { }
 
   ngOnInit() {
+    this.backend = this.hostSetting.getHost();
+  }
+
+  formatURL(endpoint: string, id?: string, format?: string) {
+    let url = this.backend + `${endpoint}/`;
+
+    if (id) {
+      url += `${id}/`;
+    }
+
+    if (format) {
+      url += `?format=${format}`;
+    }
+
+    return url
+  }
+
+  getOrganism(id?: string, format?: string) {
+    return this.formatURL('organism', id, format);
+  }
+
+  getFile(id?: string, format?: string) {
+    return this.formatURL('file', id, format);
+  }
+
+  getSpecimen(id?: string, format?: string) {
+    return this.formatURL('specimen', id, format);
   }
 
 }
